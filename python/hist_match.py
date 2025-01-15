b6c5816f25fbb2316a0a90bfeae55edc8c7340a1
Owen Puls <ow.f.puls@gmail.com>
2025-01-15 12:23:38 -0500

    hist_match

--- python/hist_match.py
+++ python/hist_match.py
@@ -15,7 +15,7 @@ def parse_args():
 
     return args
 
-def subtract_background(image, radius=50, light_bg=False):
+def subtract_background(image, radius=25, light_bg=False):
         from skimage.morphology import white_tophat, black_tophat, disk
         str_el = ball(radius) #you can also use 'ball' here to get a slightly smoother result at the cost of increased computing time
         if light_bg:
