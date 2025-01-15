221597e72e4f58492bf03348b6c952ffa58bee93
Owen Puls <ow.f.puls@gmail.com>
2025-01-15 14:02:26 -0500

    hist_match

--- python/hist_match.py
+++ python/hist_match.py
@@ -11,6 +11,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Histogram match volumes to the 3DRCAN training data')
     parser.add_argument('ref_dir', type=Path, help='path to directory containing the files to which we are matching the histogram')
     parser.add_argument('input_dir', type=Path, help='path to base directory containing the experiment we want to modify')
+    parser.add_argument('out_dir', type=str, help='name of the created save directoy')
+    parser.add_argument('--bg_sub', '-bg', default=False, action='store_true', dest='bgsub',help='path to base directory containing the experiment we want to modify')
     args = parser.parse_args()
 
     return args
@@ -26,13 +28,13 @@ def subtract_background(volume, radius=50, light_bg=False):
         # str_el = ball(radius) #you can also use 'ball' here to get a slightly smoother result at the cost of increased computing time
     if light_bg:
         for i in range(volume.shape[0]):
-            background[i] = black_tophat(volume[i], disk(radius))
+            background[i] = restoration.rolling_ball(volume[i], radius=radius)
     else:
         for i in range(volume.shape[0]):
-            background[i] = white_tophat(volume[i], disk(radius))
+            background[i] = restoration.rolling_ball(volume[i], radius=radius)
     return volume - background
 
-def hist_match(ref_ddir,ddir):
+def hist_match(ref_ddir,ddir,out_dir,bg_sub=False):
 
     im_list = []
     for nn, fname in enumerate(os.listdir(ref_ddir)):
@@ -44,7 +46,7 @@ def hist_match(ref_ddir,ddir):
     multichannel_image = np.vstack(im_list)
 
 
-    out_dir = os.path.join(ddir,f'denoised_bgsub_histmatch_16')
+    out_dir = os.path.join(ddir,out_dir)
     if not os.path.exists(out_dir): 
       
     # if the demo_folder directory is not present  
@@ -54,10 +56,13 @@ def hist_match(ref_ddir,ddir):
         ext = Path(fname).suffix
         if ext == '.tif':
             img = tifffile.imread(os.path.join(ddir,fname))
-            img_bgsub = subtract_background(img)
+            if bg_sub:
+                img_new = subtract_background(img)
+            else:
+                img_new = np.copy(img)
             # background = restoration.rolling_ball(
             #     img, kernel=disk(50))
-            img_hist_match = match_histograms(img_bgsub,multichannel_image,channel_axis=None)
+            img_hist_match = match_histograms(img_new,multichannel_image,channel_axis=None)
 
             tifffile.imwrite(os.path.join(out_dir,fname),img_hist_match.astype(np.uint16))
             print(f'Finished file ' + str(nn))
@@ -66,7 +71,7 @@ def main():
     
     args = parse_args()
     
-    hist_match(ref_ddir = args.ref_dir, ddir = args.input_dir)
+    hist_match(ref_ddir = args.ref_dir, ddir = args.input_dir,out_dir=args.out_dir,bg_sub=args.bgsub)
     
 if __name__ == '__main__':
     
